30c564ed27c443b8e704f619917c704363d61464
Peter Simeonov <peter@simeonov.de>
2022-08-01 12:10:46 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ResponseCodes: require('./ResponseCodes'),
+  MessageIds: require('./Messages'),
+  FrameParser: require('./FrameParser'),
+  FrameAssembler: require('./FrameAssembler'),
+  FrameBuilder: require('./FrameBuilder'),
+  XmeyeInterpretter: require('./Interpretter'),
+  Connection: require('./Connection'),
+  PcapReader: require('./PCapReader')
+}
